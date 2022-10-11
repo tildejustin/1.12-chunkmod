0800f4e36213bdcea5eb2df07b18e06e1452893d
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-10-11 16:52:04 +0200

    merge 1.9-1.12.2 and 1.13.2

--- src/main/java/com/kingcontaria/chunkmod/mixin/MinecraftServerMixin.java
+++ src/main/java/com/kingcontaria/chunkmod/mixin/MinecraftServerMixin.java
@@ -8,12 +8,12 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
 
-    @ModifyConstant(method = "prepareWorlds", constant = @Constant(intValue = 192))
+    @ModifyConstant(method = {"prepareWorlds", "method_20317"}, constant = @Constant(intValue = 192))
     private int chunkModOne(int constant) {
         return 0;
     }
 
-    @ModifyConstant(method = "prepareWorlds", constant = @Constant(intValue = -192))
+    @ModifyConstant(method = {"prepareWorlds", "method_20317"}, constant = @Constant(intValue = -192))
     private int chunkModTwo(int constant) {
         return 0;
     }
